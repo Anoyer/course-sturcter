d1f3d63bc5c80df9a8daaecc4ef296c7a6afabe9
anoyer <zhihuangliu@foxmail.com>
2021-04-24 21:24:54 +0800

    init repo

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import os
+from utils import common
+
+
+if __name__ == "__main__":
+    print("ok")
+
+
---
+++ utils/common.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 hch All Rights Reserved.
+# Author: chuanhuihuang@foxmail.com
+# Author 自己改
+
+import os
+import sys
+
+
