7353c35d9f82858fb17878b0b7628a7a1e3b35d4
anoyer <zhihuangliu@foxmail.com>
2021-04-24 23:57:03 +0800

    add read write

--- utils/common.py
+++ utils/common.py
@@ -4,7 +4,35 @@
 # Author: chuanhuihuang@foxmail.com
 # Author 自己改
 
+__all__ = [
+    "read_csv_from_file",
+    "write_data_to_csv"
+]
+
+
 import os
 import sys
+import csv
+import pandas as pd
+
+
+def read_csv_from_file(path_of_file):
+    if not os.path.exists(path_of_file):
+        return pd.DataFrame({})
+
+    try:
+        with open(path_of_file) as file:
+            data = pd.read_csv(file)
+        return data
+    except Exception as e:
+        print(str(e))
+        return pd.DataFrame({})
+
+
+def write_data_to_csv(data: pd, path):
+    try:
+        data.to_csv(path)
+    except Exception as e:
+        print(str(e))
 
 
